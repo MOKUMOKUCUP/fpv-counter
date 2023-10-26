d396a0772ef4127357d2bc31b50887056179cd2a
kkosuge <root@kksg.net>
2023-10-27 00:06:22 +0900

    update style

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -33,20 +33,20 @@ export default function Root() {
       <div className="text-center mt-8">
         <Link
           to="/new"
-          className="bg-black text-white shadow p-2 rounded-md mx-auto"
+          className="bg-black text-white shadow px-6 py-4 rounded-md mx-auto text-lg"
         >
           新しいゲームを作成する
         </Link>
       </div>
 
-      <div className="mt-6 flex flex-col gap-2 max-w-[480px] mx-auto px-2">
+      <div className="mt-10 flex flex-col gap-3 max-w-[480px] mx-auto px-2">
         {games.map((game) => (
-          <div key={game.id} className="flex-1">
+          <div key={game.id}>
             <Link
               to={`/games/${game.id}`}
-              className="bg-white shadow p-2 px-4 rounded-md mx-auto flex "
+              className="bg-white shadow py-4 px-6 rounded-md mx-auto flex border"
             >
-              <div>{game.name}</div>
+              <div className="text-lg">{game.name}</div>
               <div className="text-sm text-gray-400 flex items-center ml-auto">
                 <div>{format(new Date(game.ts), "yyyy/MM/dd HH:mm")}</div>
               </div>
